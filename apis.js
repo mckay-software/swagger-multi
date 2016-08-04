37a96246ca1c4c3ff261c4bafc8754ca889e921d
Félix Saparelli <felix@passcod.name>
2016-08-04 16:18:14 +1200

    Remove METS docs, use HTTPS

--- apis.js
+++ apis.js
@@ -1,5 +1,4 @@
 window.apis = [
-  {name: 'METS v3', url: 'http://mets.mckay.co.nz/api/3/docs'},
-  {name: 'SCADAfarm', url: 'http://api.scadafarm.com/docs'},
+  {name: 'SCADAfarm', url: 'https://api.scadafarm.com/docs'}
 ];
 
